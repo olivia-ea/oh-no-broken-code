11d8a69444a4fda4752574c09670d4d7e0898c4b
Olivia Ea <ea.olivia1@gmail.com>
2019-09-11 14:14:39 -0700

    Fixed github issue

--- github.py
+++ github.py
@@ -1,28 +1,19 @@
-# I'm so close with this one.. I've got it to print out some stuff I got
-# from the GitHub API, but not what I want!
-# I saved the API stuff in a file to save time! :-)
-
 import json
 
-def github_api_response():
+def github_api_response():  # string to dictionary
     with open('github.json') as f:
-        github = json.loads(f.read())
-    return github
+        github_response = json.loads(f.read())
+    return github_response
 
+def print_user_repository_names(api_response):
+    for item in api_response:
+        if item['description'] == "":
+            print(f"{item['name']}")
+        else:
+            print(f"{item['name']}: {item['description']}")
 
-def print_user_repository_names():
-
-    # I just want to print out the names and descriptions of the repositories..
-    # like 'Hackbright-Curriculum: Exercises for the Hackbright Academy Fellowship Program'
-
-    repos = github_api_response()
-
-    for repo in repos:
-        # I don't think I have these keys right
-        # Also I'd like to print it on one line.
-        print repo['repo_name']
-        print repo['repo_description']
 
 
 if __name__=="__main__":
-    print_user_repository_names()
+    github_response = github_api_response()
+    print_user_repository_names(github_response)
