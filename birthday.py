57e50d25e02c1b8d36837dbab67811bea5ba6e2e
Olivia Ea <ea.olivia1@gmail.com>
2019-09-11 12:09:14 -0700

    Fixed birthday file bugs

--- birthday.py
+++ birthday.py
@@ -1,34 +1,31 @@
 from datetime import datetime
 
-number_endings = {
-    1: 'st',
-    2: 'nd',
-    3: 'rd'
-}
+def find_ending(day):
+    number_endings = {
+        1: 'st',
+        2: 'nd',
+        3: 'rd'
+    }
 
-today = datetime.now()
-todays_day = today.day
+    ending = ''
+    if 10 < day < 20:
+        ending = 'th'
+    else:
+        num = day % 10
+        if num in number_endings:
+            ending = number_endings.get(num)
+        else:
+            ending = 'th'
+    return ending
 
-# get the right ending, e.g. 1 => 1st, 2 => 2nd
-# but beware! 11 => 11th, 21 => 21st, 31 => 31st
+if __name__ == '__main__':
+    today = datetime.now()
+    todays_day = today.day
+    todays_day_ending = find_ending(todays_day)
 
-# test your code by forcing todays_day to be something different
-# todays_day = 11
+    print(f"Today is the {todays_day}{todays_day_ending}")
 
-ending = 'th'
+    birthday_day = int(input("What day of the month is your birthday?" ))
+    birthday_ending = find_ending(birthday_day)
 
-if todays_day < 10 or todays_day > 20:
-    # x % y (mod) will give the remainder when x is divided by y
-    # -- but x needs to be an integer!
-    number = todays_day % 10
-    # look up number in number_endings
-    # and if you find it as a key, put the value in ending
-    if number in number_endings:
-
-# make this print ending, not 'th'
-print "Today is the {}th".format(todays_day)
-
-birthday = int(raw_input("What day of the month is your birthday?"))
-
-# make this print the birthday, and the right ending
-print "Your birthday is on the {}th!".format(todays_day)
+    print(f"Your birthday is on the {birthday_day}{birthday_ending}!")
