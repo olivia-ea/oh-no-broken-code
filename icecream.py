d9bee63a2f5f5ff375acf9e223e19a71bcf8932f
Olivia Ea <ea.olivia1@gmail.com>
2019-09-11 12:15:56 -0700

    Fixed icecream file bugs

--- icecream.py
+++ icecream.py
@@ -1,11 +1,9 @@
 # I want to print out my favorite ice cream flavors.
 
-all_flavors = ['chocolate', 'mint', 'strawberry', 'caramel', 'pecan',
-               'cookie dough', 'vanilla', 'lemon']
-my_faves    = ['mint', 'caramel']
+all_flavors = {'chocolate', 'mint', 'strawberry', 'caramel', 'pecan',
+               'cookie dough', 'vanilla', 'lemon'}
+my_faves = {'mint', 'caramel'}
 
-
-for item in my_list:
-    if my_faves:
-        print "I like {}".format(item)
-
+for flavor in all_flavors:
+    if flavor in my_faves:
+        print(f"I like {flavor}!")
